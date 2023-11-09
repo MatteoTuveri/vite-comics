df5c0057031da1ea9057b76af61f5047d9a97a7d
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-11-09 17:16:59 +0100

    Add content and sections area in main component

--- src/components/main/ContentComp.vue
+++ src/components/main/ContentComp.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-
+        <h1>
+            {{ title }}
+        </h1>
     </div>
 </template>
   
@@ -8,10 +10,21 @@
 export default {
     data() {
         return {
-
+            title:'--> Content Goes Here <--'
         }
     }
 }
 </script>
   
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+div{
+    background-color: rgb(28, 28, 28);
+    padding: 40px 0;
+}
+
+h1{
+    margin: 0 auto;
+    width: 60%;
+    color: white;
+}
+</style>
--- src/components/main/SectionsAreaComp.vue
+++ src/components/main/SectionsAreaComp.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
+    <div class="bg">
+        <ul>
+            <li v-for="(item,index) in icons">
+                <div class="img">
+                    <img :src="item.image" :alt="item.title">
+                </div>
+                <div class="title">
+                    {{ item.title }}
+                </div>
+            </li>
+        </ul>
 
     </div>
 </template>
@@ -8,10 +18,60 @@
 export default {
     data() {
         return {
-
+            icons:[
+                {
+                    image:'/Images/buy-comics-digital-comics.png',
+                    title:'digital comics'
+                },
+                {
+                    image:'/Images/buy-comics-merchandise.png',
+                    title:'dc merchandise'
+                },
+                {
+                    image:'/Images/buy-comics-subscriptions.png',
+                    title:'subscription'
+                },
+                {
+                    image:'/Images/buy-comics-shop-locator.png',
+                    title:'comic shop locator'
+                },
+                {
+                    image:'/Images/buy-dc-power-visa.svg',
+                    title:'dc power visa'
+                },
+            ]
         }
     }
 }
 </script>
-  
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+ul{
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    width: 60%;
+    margin: 0 auto;
+}
+li{
+    display: flex;
+    align-items: center;
+}
+
+img{
+    width: 100%;
+    padding: 0 10px;
+}
+
+.img{
+    width: 80px;
+}
+.title{
+    text-transform: uppercase;
+    color: white;
+}
+
+.bg{
+    background-color: rgb(2, 130, 249);
+    padding: 40px;
+}
+</style>
